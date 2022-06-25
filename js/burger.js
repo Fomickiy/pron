63399778376dba8b5947585280c208260014dae8
Fomickiy <bogdantt285@gmail.com>
2022-06-25 14:43:15 +0000

    css

---
+++ js/burger.js
@@ -0,0 +1,21 @@
+(function () {
+    const iconBurger = document.querySelector(".search_content");
+    const navigationsBody = document.querySelector(".header-nav__wrapper");
+    iconBurger.addEventListener("click", function (e) {
+      document.body.classList.toggle("lock");
+      iconBurger.classList.toggle("active");
+      navigationsBody.classList.toggle("active");
+    });
+  
+    const navLinks = document.querySelectorAll(".header-nav__menu-item");
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("click", (_) => {
+            if (iconBurger.classList.contains("active")) {
+                document.body.classList.remove("lock");
+                iconBurger.classList.remove("active");
+                navigationsBody.classList.remove("active");
+            }
+          }
+      );
+    });
+  })();
